669ff854f790aca091b9f36f77efc142b6d024e1
JoseFlavio <jflavio90@gmail.com>
2017-07-25 00:36:14 -0500

    adding header

--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
+__author__ = "Jose Flavio Quispe Irrazabal"
+__copyright__ = "Copyright 2017, Laboratorio 4"
+__license__ = "Apache"
+__version__ = "1.0"
+__email__ = "jflavio90@gmail.com"
+__status__ = "Production"
+
 from BagOfVW import BagOfVW
 
 def init():
